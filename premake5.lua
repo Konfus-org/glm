d1e79540af1e1d0fc99b637d2a3e77d590351594
Konfus-dev <63468146+Konfus-dev@users.noreply.github.com>
2025-01-06 20:43:47 -0800

    Removing unneeded stuffs

---
+++ premake5.lua
@@ -0,0 +1,41 @@
+project "glm"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++17"
+    staticruntime "off"
+
+    if OutputIntermediateDir == nil or OutputTargetDir == nil then
+        targetdir ("Build/bin/%{prj.name}/")
+        objdir    ("Build/obj/%{prj.name}/")
+
+    else
+        targetdir ("../../../" .. OutputTargetDir .. "")
+        objdir    ("../../../" .. OutputIntermediateDir .. "")
+    end
+
+    defines
+    {
+        "SPDLOG_COMPILED_LIB"
+    }
+
+    files
+    {
+        "glm/**.hpp",
+        "glm/**.inl",
+        "glm/glm.cpp"
+    }
+
+    includedirs { "glm" }
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
+
+    filter "configurations:Dist"
+        runtime "Release"
+        optimize "on"
+        symbols "off"
