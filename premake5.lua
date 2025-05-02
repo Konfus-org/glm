85363601ef86f3ec3b4ff76f7165424a137792fc
Konfus-dev <63468146+Konfus-dev@users.noreply.github.com>
2025-05-01 18:27:27 -0700

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -22,23 +22,25 @@ project "glm"
     {
         "glm/**.hpp",
         "glm/**.inl",
-        "glm/glm.cpp"
+        "glm/glm.cpp",
+        "./**.md",
+        "./**.lua",
     }
 
     includedirs { "glm" }
 
     filter "configurations:Debug"
         runtime "Debug"
-        linkoptions { "/LTCG:incremental" }  -- Enable Link Time Code Generation and Incremental linking
+        linkoptions { "/MDd" }  -- Enable Link Time Code Generation and Incremental linking
         symbols "on"
 
     filter "configurations:Optimized"
         runtime "Release"
-        linkoptions { "/LTCG:incremental" }  -- Enable Link Time Code Generation and Incremental linking
+        linkoptions { "/MDd" }  -- Enable Link Time Code Generation and Incremental linking
         optimize "on"
 
     filter "configurations:Release"
         runtime "Release"
         optimize "on"
-        linkoptions { "/LTCG:incremental" }  -- Enable Link Time Code Generation and Incremental linking
+        linkoptions { "/MD" }  -- Enable Link Time Code Generation and Incremental linking
         symbols "off"
